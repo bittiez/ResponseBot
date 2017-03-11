6b8daceeed52af8f8e5204b04e5a73ccc21b187e
Tad Taylor <bittiez@gmail.com>
2017-03-11 14:47:07 -0700

    Multi-Threading
    Avoiding server delays

---
+++ src/US/bittiez/ResonseBot/checkAPI.java
@@ -0,0 +1,70 @@
+package US.bittiez.ResonseBot;
+
+import org.apache.http.client.fluent.Request;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by gamer on 3/11/2017.
+ */
+public class checkAPI implements Runnable{
+    public String message;
+    public Player player;
+    public FileConfiguration config;
+    public static Logger log;
+    public Plugin plugin;
+
+    @Override
+    public void run() {
+        this.message = ChatColor.stripColor(this.message);
+
+        String API_BASE = "https://api.api.ai/api";
+        try {
+            Request res = Request.Get(API_BASE
+                    + "/query"
+                    + "?v=20150910"
+                    + "&query="
+                    + URLEncoder.encode(message, "UTF-8")
+                    + "&lang=" + config.getString("lang", "en")
+                    + "&sessionId=" + player.getUniqueId()
+            )
+                    .addHeader("Authorization", "Bearer " + config.getString("accesstoken"));
+            log.log(Level.INFO, res.toString());
+
+            String json = res.execute().returnContent().asString();
+            JSONParser parser = new JSONParser();
+            JSONObject parsed = (JSONObject)parser.parse(json);
+            JSONObject result = (JSONObject)parsed.get("result");
+            JSONObject fulfillment = (JSONObject)result.get("fulfillment");
+
+            String response = fulfillment.get("speech").toString();
+            if(response.length() > 0) {
+                if (config.getBoolean("replyToPlayer", false)) {
+                    player.sendMessage(genResponse(response));
+                } else {
+                    plugin.getServer().broadcastMessage(genResponse(response));
+                }
+            }
+
+        } catch (Exception err) {
+            log.log(Level.WARNING, "ResponseBot encountered an unknown error.");
+            err.printStackTrace();
+        }
+    }
+
+    private String genResponse(String response){
+        return ChatColor.translateAlternateColorCodes('&',
+                config.getString("chatFormat")
+                        .replace("[BOTNAME]", config.getString("botname"))
+                        .replace("[RESPONSE]", response));
+    }
+
+}
--- src/US/bittiez/ResonseBot/main.java
+++ src/US/bittiez/ResonseBot/main.java
@@ -11,7 +11,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -47,50 +46,14 @@ public class main extends JavaPlugin implements Listener{
 
     @EventHandler
     public void onPlayerChatEvent(AsyncPlayerChatEvent e){
-        String response = checkAPI(ChatColor.stripColor(e.getMessage()), e.getPlayer());
-        if(response.length() > 0) {
-            if (config.getBoolean("replyToPlayer", false)) {
-                e.getPlayer().sendMessage(genResponse(response));
-            } else {
-                getServer().broadcastMessage(genResponse(response));
-            }
-        }
-    }
+        checkAPI API = new checkAPI();
+        API.player = e.getPlayer();
+        API.message = e.getMessage();
+        API.plugin = this;
+        API.config = config;
+        API.log = log;
 
-    private String genResponse(String response){
-        return ChatColor.translateAlternateColorCodes('&',
-                config.getString("chatFormat")
-                    .replace("[BOTNAME]", config.getString("botname"))
-                    .replace("[RESPONSE]", response));
-    }
-
-    private String checkAPI(String message, Player player){
-        String API_BASE = "https://api.api.ai/api";
-        try {
-            Request res = Request.Get(API_BASE
-                    + "/query"
-                    + "?v=20150910"
-                    + "&query="
-                        + URLEncoder.encode(message, "UTF-8")
-                    + "&lang=" + config.getString("lang", "en")
-                    + "&sessionId=" + player.getUniqueId()
-            )
-                .addHeader("Authorization", "Bearer " + config.getString("accesstoken"));
-            log.log(Level.INFO, res.toString());
-
-            String json = res.execute().returnContent().asString();
-            JSONParser parser = new JSONParser();
-            JSONObject parsed = (JSONObject)parser.parse(json);
-            JSONObject result = (JSONObject)parsed.get("result");
-            JSONObject fulfillment = (JSONObject)result.get("fulfillment");
-
-            return fulfillment.get("speech").toString();
-
-        } catch (Exception err) {
-            log.log(Level.WARNING, "ResponseBot encountered an unknown error.");
-            err.printStackTrace();
-        }
-        return "";
+        new Thread(API).start();
     }
 
     private void createConfig() {
