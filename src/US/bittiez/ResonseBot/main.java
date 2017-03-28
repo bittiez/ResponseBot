560d8633ec10005c4f45fe366c5846f6c36a81a8
Tad Taylor <bittiez@gmail.com>
2017-03-27 19:40:15 -0600

    Many Changes
    Moved some files
    Added an update checker

--- src/US/bittiez/ResonseBot/main.java
+++ src/US/bittiez/ResonseBot/main.java
@@ -1,11 +1,15 @@
 package US.bittiez.ResonseBot;
 
+import US.bittiez.ResonseBot.UpdateChecker.UpdateChecker;
+import US.bittiez.ResonseBot.UpdateChecker.UpdateStatus;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,6 +19,7 @@ import java.util.logging.Logger;
 public class main extends JavaPlugin implements Listener{
     private static Logger log;
     public FileConfiguration config = getConfig();
+    public boolean hasUpdate = false;
 
     @Override
     public void onEnable() {
@@ -22,6 +27,12 @@ public class main extends JavaPlugin implements Listener{
         createConfig();
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(this, this);
+
+        UpdateStatus update = new UpdateChecker("https://github.com/bittiez/ResponseBot/raw/master/src/plugin.yml", getDescription().getVersion()).getStatus();
+        if(update.HasUpdate){
+            hasUpdate = true;
+            log.warning("ResponseBot is outdated, you can check out the update at: https://github.com/bittiez/ResponseBot/releases or https://www.spigotmc.org/resources/responsebot.37901/");
+        }
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]) {
@@ -36,6 +47,14 @@ public class main extends JavaPlugin implements Listener{
         return false;
     }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        if(hasUpdate){
+            if(e.getPlayer().isOp() || e.getPlayer().hasPermission("ResponseBot.updates")){
+                e.getPlayer().sendMessage(ChatColor.GOLD + "Your version of ResponseBot is outdated, you can check for updates at: https://github.com/bittiez/ResponseBot/releases or https://www.spigotmc.org/resources/responsebot.37901/");
+            }
+        }
+    }
     @EventHandler
     public void onPlayerChatEvent(AsyncPlayerChatEvent e){
         if(!e.isCancelled()) {
